bc8214036c736accb9fa1a7e7a7725e92fbd7e2a
JoshSmith84 <jas426@icloud.com>
2026-03-23 08:57:57 -0500

    B: added usage to stdout

--- main.py
+++ main.py
@@ -15,5 +15,10 @@ if __name__ == "__main__":
         model='gemini-2.5-flash', 
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
-
+    if response.usage_metadata != None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+              f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+    else:
+        raise RuntimeError("Failed to fetch usage metadata. " \
+                           "API call most likely failed")
     print(response.text)

a63239f2239c3d60bb197c855215bcd5e1d7d083
JoshSmith84 <jas426@icloud.com>
2026-03-23 09:44:40 -0500

    D: added verbose argument and fuinctionality. Added infrastructure to keep track of ai/user convo, but not yet implemented

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 if __name__ == "__main__":
@@ -9,8 +10,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Gemini in your CLI")
     parser.add_argument("ai_prompt", 
                         type=str, 
-                        help="Type a prompt for Google Gemini enclosed in quotes")
+                        help="Type a prompt for Google Gemini enclosed in quotes"
+                        )
+    parser.add_argument("--verbose", 
+                        action="store_true", 
+                        help="Enable verbose output"
+                        )
     args = parser.parse_args()
+
+    messages = [
+        types.Content(role="user", 
+                      parts=[types.Part(text=args.ai_prompt)]
+                      )
+        ]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,12 +33,17 @@ if __name__ == "__main__":
 
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=args.ai_prompt
+        contents=messages
     )
-    if response.usage_metadata != None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
-              f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
-    else:
+    if response.usage_metadata == None:
         raise RuntimeError("Failed to fetch usage metadata. " \
-                           "API call most likely failed")
+                           "API call most likely failed"
+                           )
+
+    if args.verbose:
+        print(f"\nUser prompt: {args.ai_prompt}\n"
+              f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+              f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
+              )   
+
     print(response.text)
